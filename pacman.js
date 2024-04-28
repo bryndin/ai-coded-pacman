96d6396b047627c39955fdb6cb32287eb8cbdd4c
bryndin <1129396+bryndin@users.noreply.github.com>
2024-04-28 01:08:47 -0700

    Step 1, Gemeni runs out of tokens on output and generates unfinished code

--- pacman.js
+++ pacman.js
@@ -1,40 +1,34 @@
-/**
-* Made with p5play!
-* https://p5play.org
-*/
-
-// Learn more about p5play here -> https://p5play.org/learn
-
-let box;
+let pacmanX, pacmanY;
+let pacmanRadius = 20;
+let angleStart = 0.2;
+let angleEnd = TWO_PI - 0.2;
+let mouthOpen = true;
 
 function setup() {
-// creates a canvas that fills the screen
-    new Canvas();
+    createCanvas(400, 400);
+    pacmanX = width / 2;
+    pacmanY = height / 2;
 }
 
 function draw() {
-    clear(); // try removing this line and see what happens!
+    background(0);
 
-    textSize(24);
-    textAlign(CENTER);
-fill(200);
-    text('Tap to create a new sprite, drag to throw it!', canvas.w / 2, canvas.h / 2);
+    // Draw Pacman
+    fill(255, 255, 0);
+    arc(pacmanX, pacmanY, pacmanRadius * 2, pacmanRadius * 2, angleStart, angleEnd, PIE);
 
-    if (mouse.presses()) {
-//              (      x, y,    width, height)
-box = new Sprite(mouse.x, mouse.y, 30, 30);
-// by default, sprites collide with other sprites
-}
-if (mouse.dragging()) {
-box.moveTowards(mouse); // throw the box!
-}
-// if the user didn't throw the box,
-// then give it a random speed and direction
-if (mouse.released() && !mouse.dragged()) {
-box.speed = random(0, 5);
-box.direction = random(0, 360);
-}
-
-    // by default, all sprites are drawn by p5play
-    // after the end of the draw function
+    // Animate mouth
+    if (mouthOpen) {
+        angleStart += 0.05;
+        angleEnd -= 0.05;
+        if (angleStart >= TWO_PI / 3 || angleEnd <= TWO_PI - TWO_PI / 3) {
+            mouthOpen = false;
+        }
+    } else {
+        angleStart -= 0.05;
+        angleEnd += 0.05;
+        if (angleStart <= 0.2 || angleEnd >= TWO_PI - 0.2) {
+            mouthOpen = true;
+        }
+    }
 }
